05a692a8cf8191d09f21e05e6e5e62f74e5498ce
Orçun Çolak <orcun.colak@hazelcast.com>
2024-10-11 12:41:23 +0300

    fix example

--- src/main/java/com/colak/springtutorial/controller/BookController.java
+++ src/main/java/com/colak/springtutorial/controller/BookController.java
@@ -1,22 +1,23 @@
 package com.colak.springtutorial.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
-
 @RestController
+@RequestMapping("/api/books")
 public class BookController {
 
-    @GetMapping("/api/books")
-    public List<String> getBooks(@RequestParam boolean shouldFail) {
+    // http://localhost:8080/books?shouldFail=false
+    // http://localhost:8080/api/books?shouldFail=false
+    @GetMapping
+    public String getBooks(@RequestParam boolean shouldFail) {
         // If shouldFail is true, throw an exception
         if (shouldFail) {
             throw new RuntimeException("Simulated service failure due to parameter.");
         }
-        return Arrays.asList("Book 1", "Book 2", "Book 3");
+        return "Book 1 Book 2 Book 3";
     }
 
 }
--- src/test/java/com/colak/springtutorial/controller/BookControllerTest.java
+++ src/test/java/com/colak/springtutorial/controller/BookControllerTest.java
@@ -1,63 +1,31 @@
 package com.colak.springtutorial.controller;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @AutoConfigureMockMvc
 class BookControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private TestRestTemplate restTemplate;
 
-    private static final String BASE_URL = "/api/books";
-
-    @Autowired
-    private CircuitBreakerRegistry circuitBreakerRegistry;
 
     @Test
-    void testSuccess() throws Exception {
-        // Act & Assert
-        mockMvc.perform(get(BASE_URL + "?shouldFail=false")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0]").value("Book 1"))
-                .andExpect(jsonPath("$[1]").value("Book 2"))
-                .andExpect(jsonPath("$[2]").value("Book 3"));
+    void testSuccess() {
+        // Test for a successful response
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/books?shouldFail=false", String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Book 1 Book 2 Book 3", response.getBody());
     }
 
-    @Test
-    void testFailure() throws Exception {
-        // Simulate failure to trigger the circuit breaker
-        for (int i = 0; i < 3; i++) {
-            try {
-                mockMvc.perform(get(BASE_URL + "?shouldFail=true")
-                        .accept(MediaType.APPLICATION_JSON));
-            } catch (Exception exception) {
-            }
 
-        }
-
-        // Wait for the circuit breaker to open
-        // You should adjust this duration based on your circuit breaker's waitDurationInOpenState
-        Thread.sleep(6000); // Example: wait for 6 seconds if your config is set for 5 seconds
-
-        // Now that we have failed 3 times, the circuit breaker should be open.
-        // So, we expect the fallback response for the next request.
-        mockMvc.perform(get(BASE_URL + "?shouldFail=true")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Service is temporarily unavailable. Please try again later."));
-    }
 }
