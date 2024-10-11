5ed97dd335b8f2811b110ff9a1bdfaa327c51f0c
Orçun Çolak <orcun.colak@hazelcast.com>
2024-10-11 13:25:23 +0300

    fix example

--- src/test/java/com/colak/springtutorial/controller/BookControllerTest.java
+++ src/test/java/com/colak/springtutorial/controller/BookControllerTest.java
@@ -2,7 +2,6 @@ package com.colak.springtutorial.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
@@ -11,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@AutoConfigureMockMvc
 class BookControllerTest {
 
     @Autowired
